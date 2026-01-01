a45efde8d46749c1f037a1bccf3daf4430b920eb
Omar Atri <atriomar00@gmail.com>
2026-01-01 14:41:12 +0100

    Add basename to react router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -14,36 +14,41 @@ import NotFound from "@/pages/NotFound";
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 axios.defaults.withCredentials = true;
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "auth",
+          element: <AuthLayout />,
+          children: [
+            {
+              path: "login",
+              element: <Login />,
+            },
+            {
+              path: "register",
+              element: <Register />,
+            },
+          ],
+        },
+        {
+          path: "*",
+          element: <NotFound />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "auth",
-        element: <AuthLayout />,
-        children: [
-          {
-            path: "login",
-            element: <Login />,
-          },
-          {
-            path: "register",
-            element: <Register />,
-          },
-        ],
-      },
-      {
-        path: "*",
-        element: <NotFound />,
-      },
-    ],
-  },
-]);
+    basename: "/app",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AuthProvider>
